169038b1caf5d2c1b3c277f5d9cd758b2927cf07
izaz97778 <izaz97778@gmail.com>
2025-07-20 17:27:31 +0500

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") 
-    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
+    FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "False")
     PORT = environ.get('PORT', '8080')
     
 #Dont Remove My Credit @Silicon_Bot_Update 
